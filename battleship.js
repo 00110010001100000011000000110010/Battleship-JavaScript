2b5b58b095ed608065ba60647e29c56922a65281
00110010001100000011000000110010 <kolesar.andris@gmail.com>
2018-04-01 14:40:25 +0200

    Display the amount of hits and the amount of fired torpedos
    Fixed ship placement

--- battleship.js
+++ battleship.js
@@ -37,23 +37,26 @@ for (i = 0; i < cols; i++) {
       Patrol Boat - 2 hits
 */
 var hitCount = 0;
+var torpedoCount = 0;
 
 /* create the 2d array that will contain the status of each square on the board
    and place ships on the board (later, create function for random placement!)
 
    0 = empty, 1 = part of a ship, 2 = a sunken part of a ship, 3 = a missed shot
 */
+
 var gameBoard = [
 				[0,0,0,1,1,1,1,0,0,0],
 				[0,0,0,0,0,0,0,0,0,0],
 				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,1,0,0,0],
-				[0,0,0,0,0,0,1,0,0,0],
+				[0,0,1,1,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
 				[1,0,0,0,0,0,1,1,1,1],
 				[1,0,0,0,0,0,0,0,0,0],
 				[1,0,0,1,0,0,0,0,0,0],
 				[1,0,0,1,0,0,0,0,0,0],
 				[1,0,0,0,0,0,0,0,0,0]
+				
 				]
 
 // set event listener for all elements in gameboard, run fireTorpedo function when square is clicked
@@ -66,6 +69,8 @@ function fireTorpedo(e) {
         // extract row and column # from the HTML element's id
 		var row = e.target.id.substring(1,2);
 		var col = e.target.id.substring(2,3);
+		torpedoCount++;
+		document.getElementById("torpedoCount").innerHTML = torpedoCount;
         //alert("Clicked on row " + row + ", col " + col);
 				
 		// if player clicks a square with no ship, change the color and change square's value
@@ -73,15 +78,16 @@ function fireTorpedo(e) {
 			e.target.style.background = '#bbb';
 			// set this square's value to 3 to indicate that they fired and missed
 			gameBoard[row][col] = 3;
-			
 		// if player clicks a square with a ship, change the color and change square's value
 		} else if (gameBoard[row][col] == 1) {
 			e.target.style.background = 'red';
 			// set this square's value to 2 to indicate the ship has been hit
 			gameBoard[row][col] = 2;
+
 			
 			// increment hitCount each time a ship is hit
 			hitCount++;
+			document.getElementById("hitCount").innerHTML = hitCount;
 			// this definitely shouldn't be hard-coded, but here it is anyway. lazy, simple solution:
 			if (hitCount == 17) {
 				alert("All enemy battleships have been defeated! You win!");
